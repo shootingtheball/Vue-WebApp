796b5391a629d0456d34eabe1508d2e014cf584f
张廷玉 <412753988@qq.com>
2017-11-21 16:01:03 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <vfooter></vfooter>
+  </div>
+</template>
+
+<script>
+import vheader from "./components/vheader/vheader";
+import vfooter from "./components/vfooter/vfooter";
+export default {
+  name: 'app',
+  data() {
+    return{
+      onelist:{}
+    }
+  },
+  components:{
+    vfooter
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  font-family: "PingFangSC-Light",sans-serif;
+}
+#app {
+  text-align: center;
+  color: #2c3e50;
+}
+body{
+  position: relative;
+}
+</style>
